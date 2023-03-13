fff902bf1b066368a7879ff7ea556052f1f41341
Shohidul Arifin <shohidularifin@gmail.com>
2023-03-13 11:20:50 -0700

    object based notation added

---
+++ notation.js
@@ -0,0 +1,12 @@
+person ={
+    name: "karim khan",
+    age: 35,
+    65: 'house',
+    'phone-number': 12345,
+}
+
+console.log(person.name);
+console.log(person['age']);
+
+console.log(person[65])
+console.log(person['phone-number'])
