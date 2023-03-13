af81ddf635c3f19bbfe06fa00edf5e2289485195
Shohidul Arifin <shohidularifin@gmail.com>
2023-03-13 05:16:37 -0700

    shortcut.js added

---
+++ shortcut.js
@@ -0,0 +1,44 @@
+// check truthy
+let myVar = 5;
+
+if(myVar){
+    myVar = myVar *100;
+}
+else{
+    myVar =0;
+}
+
+const money = 80;
+let food;
+if(money >100){
+    food = 'biryani'
+}
+else{
+    food = 'cha biscuit'
+}
+
+// ternary
+let food1 = money >100 ? 'biryani' : 'cha biscuit';
+
+let drink = (money >100 && myVar > 100)? 'coke' : 'filter water'
+
+// number to string conversion
+const num1 = 52;
+console.log( num1);
+const numStr = num1 + '';
+console.log(typeof numStr);
+
+// string to number
+const input = '560';
+const inputNum = +input
+console.log(inputNum);
+
+const isActive = true;
+const showUser =() => console.log('display user');
+const hideUser = () => console.log('hide user');
+isActive ? showUser() : hideUser();
+
+// use && if the left side is true then right side will be executed
+isActive && showUser();
+// use || if the left side is false then right side will be  executed
+isActive ||
